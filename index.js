d7f371606f84f6c87307036ee479f3ffe83e1651
Muhammad Bakhtawar <mbakhtawar.sce@gmail.com>
2024-02-18 12:29:51 +0500

    My First Js Assignment

---
+++ index.js
@@ -0,0 +1,4 @@
+alert("The weight per kg Chicken is 500");
+const chickenperkg = 500;
+const userinput = prompt("How much kg do you want to buy.Please enter your value in KG.");
+document.write(`<h1>The price of your chicken is :${chickenperkg*userinput}</h1>`);
