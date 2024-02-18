f173b8a061fff31e25e51698c2be0431f7c41f2a
Muhammad Bakhtawar <mbakhtawar.sce@gmail.com>
2024-02-18 12:42:36 +0500

    Addition

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 alert("The weight per kg Chicken is 500");
 const chickenperkg = 500;
 const userinput = prompt("How much kg do you want to buy.Please enter your value in KG.");
-document.write(`<h1>The price of your chicken is :${chickenperkg*userinput}</h1>`);
+document.write(`<center><h1>You have to pay Rs ${chickenperkg*userinput} for ${userinput} Kg of Chicken. </h1></center>`);
